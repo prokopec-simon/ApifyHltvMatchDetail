20e1c09a57e7984695445a15e00f79ef85750461
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-05-05 12:17:44 +0200

    Trying to obtain input content

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-import { Actor } from 'apify';
-import { CheerioCrawler } from 'crawlee';
+import { Actor } from "apify";
+import { CheerioCrawler } from "crawlee";
 
 await Actor.init();
 
-const input  = await Actor.getInput();
-const matchId = input.matchId;
-const matchesUrl = 'https://www.hltv.org/matches/';
+const input = await Actor.getInput();
+const inputJson = JSON.stringify(input);
+const inputObject = JSON.parse(inputJson);
+const matchId = inputObject.matchId;
+const matchesUrl = "https://www.hltv.org/matches/";
 const startUrls = [matchesUrl];
 const proxyConfiguration = await Actor.createProxyConfiguration();
 
 const crawler = new CheerioCrawler({
     proxyConfiguration,
     async requestHandler({ $ }) {
-
-    const pageContent = $('html').html();
-    await Actor.pushData({
-        input:input,
-        inputDataType: typeof input,
-        matchId: matchId,
-        content:pageContent
-    });
-    
-    }
+        const pageContent = $("html").html();
+        await Actor.pushData({
+            input: input,
+            inputDataType: typeof input,
+            matchId: matchId,
+            content: pageContent,
+        });
+    },
 });
 
 await crawler.run(startUrls);
