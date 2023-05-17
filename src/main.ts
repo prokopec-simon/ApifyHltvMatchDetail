2756832d4377359621d87223a98ddbeaf0eda0b8
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-05-17 15:05:59 +0200

    attempt to use residental proxy

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,12 @@ const inputJson = JSON.stringify(input);
 const inputObject = JSON.parse(inputJson);
 const matchId = inputObject.matchId;
 const matchesUrl = "https://www.hltv.org/matches/" + matchId + "/a";
-const oddsUrl = "https://www.hltv.org/betting/analytics/" + matchId + "/a";
-const startUrls = [matchesUrl, oddsUrl];
-const proxyConfiguration = await Actor.createProxyConfiguration();
+//const oddsUrl = "https://www.hltv.org/betting/analytics/" + matchId + "/a";
+const startUrls = [matchesUrl];
+const proxyConfiguration = await Actor.createProxyConfiguration({
+    groups: ["RESIDENTIAL"],
+    countryCode: "PL",
+});
 
 const crawler = new CheerioCrawler({
     proxyConfiguration,
